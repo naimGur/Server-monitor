fff842c9d5235ae47b496b9510ac36a27ec6eae3
naim gurleyuk <naimgur@gmail.com>
2018-07-11 18:18:57 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,36 +1,56 @@
 import socket
 import time
-from random import randint
+import sys
+import subprocess
+import re
+
+def diskUsage():
+
+    output = subprocess.run("df --total |grep total", shell=True, stdout=subprocess.PIPE,)
+    index = 0
+    data = str
+
+    while True:
+        data = str(output.stdout).split(" ")
+
+        if re.match('^[-+]?[0-9]\d{0,2}(\.\d{1,2})?%?$', str(data[index])):
+            break
+        else:
+            index+=1
+    
+    return data[index]
 
 def Main():
     host = '192.168.15.141'
     port = 5000
-
     s = socket.socket()
     while True:
         try:
             s.connect((host, port))
             break
         except:
-            print "connection error: (s.connect((host,port))"
+            sys.exit ("connection error: (s.connect((host,port))")
 
 
-    # database operation
-    temper  = '10'
+    # database operations
+    temper  = diskUsage()
     while True:
-        #try:
-        s.send(temper)
-        #    break
-        #except:
-        #    print "Data send error (s.send(temper))"
-        #try:
-        data = s.recv(1024)
-        #    break
-        #except:
-        #    print "No response data (data = s.recv(1024))"
 
-        print 'Received from server: ' + str(data)
+        try:
+            s.send(temper.encode())
+        except:
+            sys.exit("Data send error 's.send(temper)'")
+
+
+        try:
+            data = s.recv(1024).decode()
+        except:
+            print ("No response data (data = s.recv(1024))")
+
+
+        print ('Received from server: ' + str(data))
         time.sleep(1)
+
     s.close()
 
 if __name__ == '__main__':
